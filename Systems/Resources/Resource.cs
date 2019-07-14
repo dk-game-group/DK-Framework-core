34357bd34421ca9a4675818750cb7e5c3962bbd5
DrogerKerchva <shaokan.hammer@gmail.com>
2019-07-14 20:43:36 +0300

    Resourses subsystem

---
+++ Systems/Resources/Resource.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DK
+{
+    public struct Resource<T>
+    {
+        public event System.Action<T> OnValueChanged;
+
+        T _value;
+        public T value
+        {
+            get => _value;
+            set => UpdateValue(value);
+        }
+
+        void UpdateValue(T newValue)
+        {
+            if (!newValue.Equals(_value))
+            {
+                _value = newValue;
+                OnValueChanged(newValue);
+            }
+        }
+    }    
+}
